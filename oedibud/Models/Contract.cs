488c584c1d29d01442edc80d1d91e4daf13ba0b7
Roland Zechner <roland@zechner.it>
2026-04-09 23:13:00 +0200

    UI Improvements

--- oedibud/Models/Contract.cs
+++ oedibud/Models/Contract.cs
@@ -17,23 +17,6 @@ public class Contract
     // Navigation: payments assigned to this contract (many-to-many via ContractPayment)
     public List<ContractPayment> Payments { get; set; } = new();
 
-    //public decimal GetSalleryAtSpecificMonth(int year, int month)
-    //{
-    //    var date = new DateTime(year, month, 1);
-    //    var levelAtDate = CalculateLevelAtDate(date);
-    //    var sal = new TvLSalaryService();
-    //    Console.WriteLine(levelAtDate);
-    //    return sal.GetSalary(Group, levelAtDate);
-    //}
-    //private int CalculateLevelAtDate(DateTime date)
-    //{
-    //    int monthsSinceHire = (date.Year - HireDate.Year) * 12 + (date.Month - HireDate.Month) + ExperienceMonth;
-    //    for (int lvl = LevelThresholds.Length; lvl >= 1; lvl--)
-    //    {
-    //        int threshold = LevelThresholds[Math.Max(0, lvl - 1)];
-    //        if (monthsSinceHire >= threshold) return Math.Min(lvl, LevelThresholds.Length);
-    //    }
-    //    return 1;
-    //}
+
 }
 
